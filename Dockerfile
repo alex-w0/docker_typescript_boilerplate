b2030388860a9e5344c2586ed8b500f20d171f4b
Alex Wieland <wieland.alex1996@gmail.com>
2020-03-03 21:12:26 +0100

    optimize docker

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,5 @@ WORKDIR /usr/src/app
 COPY package.json ./
 RUN yarn
 COPY . .
+
+CMD [ "yarn", "start" ]
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 var express = require("express");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
 let app = express();
 
 app.get("/", function(req: any, res: any) {
-  res.send("Hello World15");
+  res.send("Hello World");
 });
 
 const server = app.listen(PORT, () => {
